cd91a95a80c7c977e0ba08f92d0480b8a447f6c1
lihuijie <huijie.li@greencloud.com.cn>
2024-11-29 23:26:06 +0800

    feat: 添加 Host 注解以动态设置主机地址

---
+++ src/main/java/com/lidachui/simpleRequest/annotation/Host.java
@@ -0,0 +1,15 @@
+package com.lidachui.simpleRequest.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Host
+ *
+ * @author: lihuijie
+ * @date: 2024/11/29 23:23
+ * @version: 1.0
+ */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Host {}
--- src/main/java/com/lidachui/simpleRequest/resolver/DefaultRequestBuilder.java
+++ src/main/java/com/lidachui/simpleRequest/resolver/DefaultRequestBuilder.java
@@ -1,19 +1,17 @@
 package com.lidachui.simpleRequest.resolver;
 
-import com.lidachui.simpleRequest.annotation.BodyParam;
-import com.lidachui.simpleRequest.annotation.HeaderParam;
-import com.lidachui.simpleRequest.annotation.PathVariable;
-import com.lidachui.simpleRequest.annotation.QueryParam;
-import com.lidachui.simpleRequest.annotation.RestRequest;
+import com.lidachui.simpleRequest.annotation.*;
 import com.lidachui.simpleRequest.util.AnnotationParamExtractor;
 import com.lidachui.simpleRequest.util.ParamInfo;
 import com.lidachui.simpleRequest.util.RequestAnnotationParser;
 
+import javafx.util.Pair;
 import lombok.extern.slf4j.Slf4j;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -37,8 +35,9 @@ public class DefaultRequestBuilder implements RequestBuilder {
         Map<Class<? extends Annotation>, Map<String, ParamInfo>> params = getParams(method, args);
 
         // 构建 URL 和 Query 参数
-        String fullUrl = constructUrl(baseUrl, restRequest, params);
 
+        Pair<String, Map<String, String>> urlPair = constructUrl(baseUrl, restRequest, params);
+        String fullUrl = urlPair.getKey();
         // 构建 Header 参数
         Map<String, String> headers = constructHeaders(restRequest.headers(), params);
 
@@ -51,7 +50,7 @@ public class DefaultRequestBuilder implements RequestBuilder {
         request.setMethod(restRequest.method());
         request.setHeaders(headers);
         request.setBody(body);
-
+        request.setQueryParams(urlPair.getValue());
         logRequestDetails(request);
         return request;
     }
@@ -59,77 +58,103 @@ public class DefaultRequestBuilder implements RequestBuilder {
     private Map<Class<? extends Annotation>, Map<String, ParamInfo>> getParams(
             Method method, Object[] args) {
 
-        Map<Class<? extends Annotation>, Function<Annotation, String>> annotationTypeMap = new HashMap<>();
-        annotationTypeMap.put(PathVariable.class, annotation -> ((PathVariable) annotation).value());
+        Map<Class<? extends Annotation>, Function<Annotation, String>> annotationTypeMap =
+                new HashMap<>();
+        annotationTypeMap.put(
+                PathVariable.class, annotation -> ((PathVariable) annotation).value());
         annotationTypeMap.put(QueryParam.class, annotation -> ((QueryParam) annotation).value());
         annotationTypeMap.put(HeaderParam.class, annotation -> ((HeaderParam) annotation).value());
         annotationTypeMap.put(BodyParam.class, annotation -> "body");
-
+        annotationTypeMap.put(Host.class, annotation -> "host");
         return AnnotationParamExtractor.extractParamsWithTypes(method, args, annotationTypeMap);
     }
 
-    private String constructUrl(String baseUrl, RestRequest restRequest,
-                                Map<Class<? extends Annotation>, Map<String, ParamInfo>> params) {
+    private Pair<String, Map<String, String>> constructUrl(
+            String baseUrl,
+            RestRequest restRequest,
+            Map<Class<? extends Annotation>, Map<String, ParamInfo>> params) {
 
         // 替换路径变量
         Set<String> consumedKeys = new HashSet<>();
-        String path = replacePlaceholders(
-                restRequest.path(),
-                params.getOrDefault(PathVariable.class, Collections.emptyMap()),
-                consumedKeys);
+        String path =
+                replacePlaceholders(
+                        restRequest.path(),
+                        params.getOrDefault(PathVariable.class, Collections.emptyMap()),
+                        consumedKeys);
 
         // 解析注解上的 Query 参数并合并动态参数
-        Map<String, String> queryParams = mergeQueryParams(
-                restRequest.queryParams(),
-                params.getOrDefault(QueryParam.class, Collections.emptyMap()),
-                consumedKeys);
+        Map<String, String> queryParams =
+                mergeQueryParams(
+                        restRequest.queryParams(),
+                        params.getOrDefault(QueryParam.class, Collections.emptyMap()),
+                        consumedKeys);
 
+        Map<String, ParamInfo> hostParams = params.getOrDefault(Host.class, Collections.emptyMap());
+        AtomicReference<String> host = new AtomicReference<>(baseUrl);
+        if (!hostParams.isEmpty()) {
+            Optional<ParamInfo> first = hostParams.values().stream().findFirst();
+            first.ifPresent(
+                    paramInfo -> {
+                        String newHost = paramInfo.getValue().toString();
+                        host.set(newHost);
+                    });
+        }
         // 构建完整 URL
-        return buildUrl(baseUrl, path, queryParams);
+        return new Pair<>(buildUrl(host.get(), path, queryParams), queryParams);
     }
 
     private Map<String, String> mergeQueryParams(
-            String[] staticQueryParams, Map<String, ParamInfo> dynamicParams, Set<String> consumedKeys) {
+            String[] staticQueryParams,
+            Map<String, ParamInfo> dynamicParams,
+            Set<String> consumedKeys) {
 
         // 解析静态 Query 参数
-        Map<String, String> queryParams = RequestAnnotationParser.parseQueryParams(staticQueryParams);
+        Map<String, String> queryParams =
+                RequestAnnotationParser.parseQueryParams(staticQueryParams);
 
         // 替换占位符
-        queryParams.replaceAll((key, value) -> replacePlaceholders(value, dynamicParams, consumedKeys));
+        queryParams.replaceAll(
+                (key, value) -> replacePlaceholders(value, dynamicParams, consumedKeys));
 
         // 添加未消费的动态参数
-        dynamicParams.forEach((key, paramInfo) -> {
-            if (!consumedKeys.contains(key)) {
-                queryParams.putIfAbsent(key, paramInfo.getValue().toString());
-            }
-        });
+        dynamicParams.forEach(
+                (key, paramInfo) -> {
+                    if (!consumedKeys.contains(key)) {
+                        queryParams.putIfAbsent(key, paramInfo.getValue().toString());
+                    }
+                });
 
         return queryParams;
     }
 
     private Map<String, String> constructHeaders(
-            String[] staticHeaders, Map<Class<? extends Annotation>, Map<String, ParamInfo>> params) {
+            String[] staticHeaders,
+            Map<Class<? extends Annotation>, Map<String, ParamInfo>> params) {
 
         // 解析静态 Header 参数
         Map<String, String> headers = RequestAnnotationParser.parseHeaders(staticHeaders);
 
         // 动态替换 Header 参数
-        Map<String, ParamInfo> headerParams = params.getOrDefault(HeaderParam.class, Collections.emptyMap());
+        Map<String, ParamInfo> headerParams =
+                params.getOrDefault(HeaderParam.class, Collections.emptyMap());
         Set<String> consumedKeys = new HashSet<>();
         headers.replaceAll((key, value) -> replacePlaceholders(value, headerParams, consumedKeys));
 
         // 添加未消费的动态 Header 参数
-        headerParams.forEach((key, paramInfo) -> {
-            if (!consumedKeys.contains(key)) {
-                headers.putIfAbsent(key, paramInfo.getValue().toString());
-            }
-        });
+        headerParams.forEach(
+                (key, paramInfo) -> {
+                    if (!consumedKeys.contains(key)) {
+                        headers.putIfAbsent(key, paramInfo.getValue().toString());
+                    }
+                });
 
         return headers;
     }
 
-    private Object extractBodyParam(Map<Class<? extends Annotation>, Map<String, ParamInfo>> params) {
-        Map<String, ParamInfo> bodyParams = params.getOrDefault(BodyParam.class, Collections.emptyMap());
+    private Object extractBodyParam(
+            Map<Class<? extends Annotation>, Map<String, ParamInfo>> params) {
+        Map<String, ParamInfo> bodyParams =
+                params.getOrDefault(BodyParam.class, Collections.emptyMap());
         return bodyParams.isEmpty() ? null : bodyParams.values().iterator().next().getValue();
     }
 
@@ -149,7 +174,10 @@ public class DefaultRequestBuilder implements RequestBuilder {
         while (matcher.find()) {
             result.append(template, cursor, matcher.start());
             String key = matcher.group(1);
-            String replacement = params.containsKey(key) ? params.get(key).getValue().toString() : matcher.group();
+            String replacement =
+                    params.containsKey(key)
+                            ? params.get(key).getValue().toString()
+                            : matcher.group();
             result.append(replacement);
 
             if (params.containsKey(key)) {
@@ -165,10 +193,12 @@ public class DefaultRequestBuilder implements RequestBuilder {
     private String buildUrl(String baseUrl, String path, Map<String, String> queryParams) {
         StringBuilder urlBuilder = new StringBuilder(baseUrl).append(path);
         if (!queryParams.isEmpty()) {
-            urlBuilder.append("?")
-                    .append(queryParams.entrySet().stream()
-                            .map(entry -> entry.getKey() + "=" + entry.getValue())
-                            .collect(Collectors.joining("&")));
+            urlBuilder
+                    .append("?")
+                    .append(
+                            queryParams.entrySet().stream()
+                                    .map(entry -> entry.getKey() + "=" + entry.getValue())
+                                    .collect(Collectors.joining("&")));
         }
         return urlBuilder.toString();
     }
